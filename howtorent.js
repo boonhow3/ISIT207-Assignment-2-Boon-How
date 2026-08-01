07603600282a14f60e8a7ff4c871648d15797a52
boonhow3 <69455439+boonhow3@users.noreply.github.com>
2026-08-01 17:59:28 +0800

    Update howtorent.js

--- howtorent.js
+++ howtorent.js
@@ -20,5 +20,6 @@ window.onload = function () {
 
 function logout() {
     sessionStorage.clear(); //clears all session data
+    alert("Logout Successful!");
     window.location.href = "index.html"; //goes back to home screen
 }
